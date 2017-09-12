55feaf6c26d26ab5b9a4450bc0ca2a7071675195
Carsten Wickner <Carsten.Wickner@gmx.de>
2017-09-12 23:41:15 +0200

    Replace tabs with spaces in new test class

--- scitos.hmx/scitos.hmx.view/src/test/java/org/hmx/scitos/hmx/view/swing/HmxViewProjectTest.java
+++ scitos.hmx/scitos.hmx.view/src/test/java/org/hmx/scitos/hmx/view/swing/HmxViewProjectTest.java
@@ -34,65 +34,65 @@ import org.hmx.scitos.hmx.view.swing.elements.SynProposition;
 /** UI test for a simple HermeneutiX project workflow. */
 public class HmxViewProjectTest extends AbstractScitosUiTest {
 
-	/**
-	 * Test of the input view of a newly created HermeneutiX project with the following steps:
-	 * <ol>
-	 * <li>create a new/empty HermeneutiX project,</li>
-	 * <li>enter a single character as origin text,</li>
-	 * <li>trigger "Undo",</li>
-	 * <li>trigger "Redo".</li>
-	 * </ol>
-	 */
-	@Test
-	public void testUndoRedoOnInputView() {
-		// #1 create a new/empty HermeneutiX project
-		this.createNewFile(FileType.HMX);
-		this.projectTree.requireSelection(HmxMessage.PROJECT_UNSAVED.get());
-		// #2 enter a single character as origin text
-		final JTextComponentFixture originTextPane = this.frame.textBox("Origin Text Input");
-		originTextPane.enterText("X");
-		// #3 trigger "Undo"
-		final JButtonFixture undoButton = this.getUndoToolBarButton();
-		final JButtonFixture redoButton = this.getRedoToolBarButton();
-		redoButton.requireDisabled();
-		undoButton.requireEnabled().click().requireDisabled();
-		originTextPane.requireText("");
-		// #4 trigger "Redo"
-		redoButton.requireEnabled().click().requireDisabled();
-		originTextPane.requireText("X");
-		undoButton.requireEnabled();
-	}
+    /**
+     * Test of the input view of a newly created HermeneutiX project with the following steps:
+     * <ol>
+     * <li>create a new/empty HermeneutiX project,</li>
+     * <li>enter a single character as origin text,</li>
+     * <li>trigger "Undo",</li>
+     * <li>trigger "Redo".</li>
+     * </ol>
+     */
+    @Test
+    public void testUndoRedoOnInputView() {
+        // #1 create a new/empty HermeneutiX project
+        this.createNewFile(FileType.HMX);
+        this.projectTree.requireSelection(HmxMessage.PROJECT_UNSAVED.get());
+        // #2 enter a single character as origin text
+        final JTextComponentFixture originTextPane = this.frame.textBox("Origin Text Input");
+        originTextPane.enterText("X");
+        // #3 trigger "Undo"
+        final JButtonFixture undoButton = this.getUndoToolBarButton();
+        final JButtonFixture redoButton = this.getRedoToolBarButton();
+        redoButton.requireDisabled();
+        undoButton.requireEnabled().click().requireDisabled();
+        originTextPane.requireText("");
+        // #4 trigger "Redo"
+        redoButton.requireEnabled().click().requireDisabled();
+        originTextPane.requireText("X");
+        undoButton.requireEnabled();
+    }
 
-	/**
-	 * Test of the analysis view of a newly created HermeneutiX project with the following steps:
-	 * <ol>
-	 * <li>create a new/empty HermeneutiX project,</li>
-	 * <li>enter a single character as origin text,</li>
+    /**
+     * Test of the analysis view of a newly created HermeneutiX project with the following steps:
+     * <ol>
+     * <li>create a new/empty HermeneutiX project,</li>
+     * <li>enter a single character as origin text,</li>
      * <li>start analysis,</li>
      * <li>ignore project info input dialog,</li>
      * <li>enter single character as translation for first Proposition,</li>
      * <li>enter single character as label for first Proposition,</li>
-	 * <li>trigger "Undo" once,</li>
-	 * <li>trigger "Undo" a second time,</li>
-	 * <li>trigger "Redo" once,</li>
-	 * <li>trigger "Redo" a second time.</li>
-	 * </ol>
-	 */
-	@Test
-	public void testUndoRedoOnAnalysisView() {
-		// #1 create a new/empty HermeneutiX project
-		this.createNewFile(FileType.HMX);
-		this.projectTree.requireSelection(HmxMessage.PROJECT_UNSAVED.get());
-		// #2 enter a single character as origin text
-		final JTextComponentFixture originTextPane = this.frame.textBox("Origin Text Input").enterText("X");
+     * <li>trigger "Undo" once,</li>
+     * <li>trigger "Undo" a second time,</li>
+     * <li>trigger "Redo" once,</li>
+     * <li>trigger "Redo" a second time.</li>
+     * </ol>
+     */
+    @Test
+    public void testUndoRedoOnAnalysisView() {
+        // #1 create a new/empty HermeneutiX project
+        this.createNewFile(FileType.HMX);
+        this.projectTree.requireSelection(HmxMessage.PROJECT_UNSAVED.get());
+        // #2 enter a single character as origin text
+        final JTextComponentFixture originTextPane = this.frame.textBox("Origin Text Input").enterText("X");
         // #3 start analysis
-		this.getButtonByText(HmxMessage.TEXTINPUT_START_BUTTON).click();
+        this.getButtonByText(HmxMessage.TEXTINPUT_START_BUTTON).click();
         // #4 ignore project info input dialog
         this.getButtonByText(Message.CANCEL).click();
         // #5 enter single character as translation for first Proposition
         this.getSynPropositionTranslationInput(0).enterText("T");
-		final JButtonFixture undoButton = this.getUndoToolBarButton().requireDisabled();
-		final JButtonFixture redoButton = this.getRedoToolBarButton().requireDisabled();
+        final JButtonFixture undoButton = this.getUndoToolBarButton().requireDisabled();
+        final JButtonFixture redoButton = this.getRedoToolBarButton().requireDisabled();
         // #6 enter single character as label for first Proposition
         this.getSynPropositionLabelInput(0).enterText("L");
         // #7 trigger "Undo" once
